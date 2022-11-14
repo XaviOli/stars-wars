1f9b58b0601c0d36b0e88ff771fa83356babc702
Javier Diaz <javier.diazoliviero@gmail.com>
2022-11-14 18:38:53 +0100

    Ardo home image

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -18,19 +18,19 @@ const Navbar = () => {
         <nav>
         <div className="nav-social flex items-center justify-center my-1.5">
           <a href="http://facebook.com">
-            <FaFacebook className="link" />
+            <FaFacebook className="link mr-3 ml-1" />
           </a>
           <> </>
           <a href="http://twitter.com">
-            <FaTwitter className="link" />
+            <FaTwitter className="link mr-3 ml-1" />
           </a>
           <> </>
           <a href="http://instagram.com">
-            <FaInstagram className="link" />
+            <FaInstagram className="link mr-3 ml-1" />
           </a>
           <> </>
           <a href="http://youtube.com">
-            <FaYoutube className="link" />
+            <FaYoutube className="link mr-3 ml-1" />
           </a>
         </div>
         </nav>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -25,19 +25,19 @@ export default function Home() {
       <footer>
         <div className="footer-links flex items-center justify-center my-1.5">
           <a href="http://facebook.com">
-            <FaFacebook className="link" />
+            <FaFacebook className="link mr-3 ml-3" />
           </a>
           <> </>
           <a href="http://twitter.com">
-            <FaTwitter className="link" />
+            <FaTwitter className="link mr-3 ml-3" />
           </a>
           <> </>
           <a href="http://instagram.com">
-            <FaInstagram className="link" />
+            <FaInstagram className="link mr-3 ml-3" />
           </a>
           <> </>
           <a href="http://youtube.com">
-            <FaYoutube className="link" />
+            <FaYoutube className="link mr-3 ml-3" />
           </a>
         </div>
         <p className="footer-text flex items-center justify-center">
